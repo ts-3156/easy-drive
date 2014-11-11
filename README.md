87c6bad918e7002232d46fd0dbbd0dd0c2c0378e
Shinohara Teruki <ts_3156@yahoo.co.jp>
2014-11-11 21:20:06 +0900

    write README

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ easy-drive
 ----
 
 I just want to copy files on google drive.
+
+```
+client = EasyDrive::Client.new
+client.copy(source_file_id, dest_folder_id, {title: 'new file name'})
+```
--- lib/easy_drive/files/copy.rb
+++ lib/easy_drive/files/copy.rb
@@ -1,6 +1,10 @@
 module EasyDrive
   module Files
-    def copy(client, drive, file_id, folder_id, options = {})
+    # @param file_id [String] source file_id
+    # @param folder_id [String] destination folder_id
+    # @param options [Hash] A customizable set of options.
+    # @option options [String] :title new file name
+    def copy(file_id, folder_id, options = {})
       client = self.client
       drive = self.drive
 
